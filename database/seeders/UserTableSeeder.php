c716ed05dad9d54b0bda8b7d42c26d167c23b48c
Toski92 <otochukwu2@gmail.com>
2023-12-01 15:09:46 +0100

    seeding data for admin, instructor and user

---
+++ database/seeders/UserTableSeeder.php
@@ -0,0 +1,46 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\DB;
+use Illuminate\Support\Facades\Hash;
+use Illuminate\Support\Str;
+
+class UserTableSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     */
+    public function run(): void
+    {
+        // Admin
+        DB::table('users')->insert([
+            [
+                'name' => 'Admin',
+                'email' => 'admin@gmail.com',
+                'password' => Hash::make('111'),
+                'role' => 'admin',
+                'status' => '1'
+            ],
+            // Instructor
+            [
+                'name' => 'Instructor',
+                'email' => 'instructor@gmail.com',
+                'password' => Hash::make('111'),
+                'role' => 'instructor',
+                'status' => '1'
+            ],
+            // User
+            [
+                'name' => 'User',
+                'email' => 'user@gmail.com',
+                'password' => Hash::make('111'),
+                'role' => 'user',
+                'status' => '1'
+            ]
+        ],
+    );
+    }
+}
