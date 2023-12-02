863bfc01d98e66d5255b11f7cdc029c95e053e0f
Toski92 <otochukwu2@gmail.com>
2023-12-03 00:05:45 +0100

    Customize login page

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -1,47 +1,147 @@
-<x-guest-layout>
-    <!-- Session Status -->
-    <x-auth-session-status class="mb-4" :status="session('status')" />
+<!doctype html>
+<html lang="en">
 
-    <form method="POST" action="{{ route('login') }}">
-        @csrf
+<head>
+	<!-- Required meta tags -->
+	<meta charset="utf-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+	<!--favicon-->
+	<link rel="icon" href="{{ asset('backend/assets/images/favicon-32x32.png') }}" type="image/png" />
+	<!--plugins-->
+	<link href="{{ asset('backend/assets/plugins/simplebar/css/simplebar.css') }}" rel="stylesheet" />
+	<link href="{{ asset('backend/assets/plugins/perfect-scrollbar/css/perfect-scrollbar.css') }}" rel="stylesheet" />
+	<link href="{{ asset('backend/assets/plugins/metismenu/css/metisMenu.min.css') }}" rel="stylesheet" />
+	<!-- loader-->
+	<link href="{{ asset('backend/assets/css/pace.min.css') }}" rel="stylesheet" />
+	<script src="{{ asset('backend/assets/js/pace.min.js') }}"></script>
+	<!-- Bootstrap CSS -->
+	<link href="{{ asset('backend/assets/css/bootstrap.min.css') }}" rel="stylesheet">
+	<link href="{{ asset('backend/assets/css/bootstrap-extended.css') }}" rel="stylesheet">
+	<link href="https://fonts.googleapis.com/css2?family=Roboto:wght@400;500&display=swap" rel="stylesheet">
+	<link href="{{ asset('backend/assets/css/app.css') }}" rel="stylesheet">
+	<link href="{{ asset('backend/assets/css/icons.css') }}" rel="stylesheet">
+	<title>Lara LMS - Login</title>
+</head>
 
-        <!-- Email Address -->
-        <div>
-            <x-input-label for="email" :value="__('Email')" />
-            <x-text-input id="email" class="block mt-1 w-full" type="email" name="email" :value="old('email')" required autofocus autocomplete="username" />
-            <x-input-error :messages="$errors->get('email')" class="mt-2" />
-        </div>
+<body class="">
+	<!--wrapper-->
+	<div class="wrapper">
+		<div class="section-authentication-cover">
+			<div class="">
+				<div class="row g-0">
 
-        <!-- Password -->
-        <div class="mt-4">
-            <x-input-label for="password" :value="__('Password')" />
+					<div class="col-12 col-xl-7 col-xxl-8 auth-cover-left align-items-center justify-content-center d-none d-xl-flex">
 
-            <x-text-input id="password" class="block mt-1 w-full"
-                            type="password"
-                            name="password"
-                            required autocomplete="current-password" />
+                        <div class="card shadow-none bg-transparent shadow-none rounded-0 mb-0">
+							<div class="card-body">
+                                 <img src="{{ asset('backend/assets/images/login-images/login-cover.svg') }}" class="img-fluid auth-img-cover-login" width="650" alt=""/>
+							</div>
+						</div>
 
-            <x-input-error :messages="$errors->get('password')" class="mt-2" />
-        </div>
+					</div>
 
-        <!-- Remember Me -->
-        <div class="block mt-4">
-            <label for="remember_me" class="inline-flex items-center">
-                <input id="remember_me" type="checkbox" class="rounded border-gray-300 text-indigo-600 shadow-sm focus:ring-indigo-500" name="remember">
-                <span class="ms-2 text-sm text-gray-600">{{ __('Remember me') }}</span>
-            </label>
-        </div>
+					<div class="col-12 col-xl-5 col-xxl-4 auth-cover-right align-items-center justify-content-center">
+						<div class="card rounded-0 m-3 shadow-none bg-transparent mb-0">
+							<div class="card-body p-sm-5">
+								<div class="">
+									<div class="mb-3 text-center">
+										<img src="{{ asset('backend/assets/images/logo-icon.png') }}" width="60" alt="">
+									</div>
+									<div class="text-center mb-4">
+										<h5 class="">Lara LMS Login</h5>
+										<p class="mb-0">Please log in to your account</p>
+									</div>
+									<div class="form-body">
+        <form method="POST" action="{{ route('login') }}" class="row g-3">
+            @csrf
+            <div class="col-12">
+                <label for="email" class="form-label">Email</label>
+                <input type="email" name="email" class="form-control @error('email')
+                    is-invalid
+                @enderror" id="email" :value="old('email')" placeholder="jhon@example.com">
+                @error('email')
+                    <span class="text-danger">{{ $message }}</span>
+                @enderror
+            </div>
+            <div class="col-12">
+                <label for="password" class="form-label">Password</label>
+                <div class="input-group" id="show_hide_password">
+                    <input type="password" name="password" class="form-control border-end-0 @error('password')
+                        is-invalid
+                    @enderror" id="password" placeholder="Enter Password"> <a href="javascript:;" class="input-group-text bg-transparent"><i class="bx bx-hide"></i></a>
+                    @error('password')
+                        <span class="text-danger">{{ $message }}</span>
+                    @enderror
+                </div>
+            </div>
+            <div class="col-md-6">
+                <div class="form-check form-switch">
+                    <input class="form-check-input" type="checkbox" id="flexSwitchCheckChecked">
+                    <label class="form-check-label" for="flexSwitchCheckChecked">Remember Me</label>
+                </div>
+            </div>
+            <div class="col-md-6 text-end">	<a href="authentication-forgot-password.html">Forgot Password ?</a>
+            </div>
+            <div class="col-12">
+                <div class="d-grid">
+                    <button type="submit" class="btn btn-primary">Sign in</button>
+                </div>
+            </div>
+            <div class="col-12">
+                <div class="text-center ">
+                    <p class="mb-0">Don't have an account yet? <a href="authentication-signup.html">Sign up here</a>
+                    </p>
+                </div>
+            </div>
+        </form>
+									</div>
+									<div class="login-separater text-center mb-5"> <span>OR SIGN IN WITH</span>
+										<hr>
+									</div>
+									<div class="list-inline contacts-social text-center">
+										<a href="javascript:;" class="list-inline-item bg-facebook text-white border-0 rounded-3"><i class="bx bxl-facebook"></i></a>
+										<a href="javascript:;" class="list-inline-item bg-twitter text-white border-0 rounded-3"><i class="bx bxl-twitter"></i></a>
+										<a href="javascript:;" class="list-inline-item bg-google text-white border-0 rounded-3"><i class="bx bxl-google"></i></a>
+										<a href="javascript:;" class="list-inline-item bg-linkedin text-white border-0 rounded-3"><i class="bx bxl-linkedin"></i></a>
+									</div>
 
-        <div class="flex items-center justify-end mt-4">
-            @if (Route::has('password.request'))
-                <a class="underline text-sm text-gray-600 hover:text-gray-900 rounded-md focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500" href="{{ route('password.request') }}">
-                    {{ __('Forgot your password?') }}
-                </a>
-            @endif
+								</div>
+							</div>
+						</div>
+					</div>
 
-            <x-primary-button class="ms-3">
-                {{ __('Log in') }}
-            </x-primary-button>
-        </div>
-    </form>
-</x-guest-layout>
+				</div>
+				<!--end row-->
+			</div>
+		</div>
+	</div>
+	<!--end wrapper-->
+	<!-- Bootstrap JS -->
+	<script src="{{ asset('backend/assets/js/bootstrap.bundle.min.js') }}"></script>
+	<!--plugins-->
+	<script src="{{ asset('backend/assets/js/jquery.min.js') }}"></script>
+	<script src="{{ asset('backend/assets/plugins/simplebar/js/simplebar.min.js') }}"></script>
+	<script src="{{ asset('backend/assets/plugins/metismenu/js/metisMenu.min.js') }}"></script>
+	<script src="{{ asset('backend/assets/plugins/perfect-scrollbar/js/perfect-scrollbar.js') }}"></script>
+	<!--Password show & hide js -->
+	<script>
+		$(document).ready(function () {
+			$("#show_hide_password a").on('click', function (event) {
+				event.preventDefault();
+				if ($('#show_hide_password input').attr("type") == "text") {
+					$('#show_hide_password input').attr('type', 'password');
+					$('#show_hide_password i').addClass("bx-hide");
+					$('#show_hide_password i').removeClass("bx-show");
+				} else if ($('#show_hide_password input').attr("type") == "password") {
+					$('#show_hide_password input').attr('type', 'text');
+					$('#show_hide_password i').removeClass("bx-hide");
+					$('#show_hide_password i').addClass("bx-show");
+				}
+			});
+		});
+	</script>
+	<!--app JS-->
+	<script src="{{ asset('backend/assets/js/app.js') }}"></script>
+</body>
+
+</html>
