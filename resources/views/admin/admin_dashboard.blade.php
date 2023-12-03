5d4becd366466e9eab518881716ffff84bad9773
Toski92 <otochukwu2@gmail.com>
2023-12-04 00:15:23 +0100

    load toast script

--- resources/views/admin/admin_dashboard.blade.php
+++ resources/views/admin/admin_dashboard.blade.php
@@ -26,6 +26,7 @@
 	<link rel="stylesheet" href="{{ asset('backend/assets/css/semi-dark.css') }}"/>
 	<link rel="stylesheet" href="{{ asset('backend/assets/css/header-colors.css') }}"/>
     <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.7.1/jquery.min.js"></script>
+    <link rel="stylesheet" type="text/css" href="https://cdnjs.cloudflare.com/ajax/libs/toastr.js/latest/toastr.css" >
 	<title>Lara LMS - Admin Dashboard</title>
 </head>
 
@@ -69,6 +70,28 @@
 	<script>
 		new PerfectScrollbar(".app-container")
 	</script>
+
+    <script type="text/javascript" src="https://cdnjs.cloudflare.com/ajax/libs/toastr.js/latest/toastr.min.js"></script>
+
+    <script>
+    @if(Session::has('message'))
+    var type = "{{ Session::get('alert-type','info') }}"
+    switch(type){
+        case 'info':
+        toastr.info(" {{ Session::get('message') }} ");
+        break;
+        case 'success':
+        toastr.success(" {{ Session::get('message') }} ");
+        break;
+        case 'warning':
+        toastr.warning(" {{ Session::get('message') }} ");
+        break;
+        case 'error':
+        toastr.error(" {{ Session::get('message') }} ");
+        break;
+    }
+    @endif
+    </script>
 </body>
 
 </html>
